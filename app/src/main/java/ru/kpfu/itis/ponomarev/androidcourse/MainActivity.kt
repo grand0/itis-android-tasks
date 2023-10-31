0d5c2eafd4907778ea1bdfa2ffac24671b21b8f5
Ilya Ponomarev <iponom0420@gmail.com>
2023-10-31 15:00:43 +0300

    app skeleton
    MainActivity, MainFragment, view binding

--- app/src/main/java/ru/kpfu/itis/ponomarev/androidcourse/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/ponomarev/androidcourse/MainActivity.kt
@@ -2,10 +2,27 @@ package ru.kpfu.itis.ponomarev.androidcourse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.kpfu.itis.ponomarev.androidcourse.databinding.ActivityMainBinding
+import ru.kpfu.itis.ponomarev.androidcourse.ui.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentContainer.id, MainFragment())
+            .commit()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
